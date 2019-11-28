0acfe345ad5c9f7bb801213cc6025e8247b5067b
nyan lin tun <devkonyan@gmail.com>
2019-11-29 05:06:11 +0630

    new : my_select feature done

--- main.rb
+++ main.rb
@@ -16,6 +16,19 @@ module Enumerable
         end
         self
     end
+
+    def my_select
+        i=0
+        new_array = []
+        while i< self.size
+            if yield(self[i] )== true
+                new_array << self[i]
+            end
+            i+=1
+        end
+        new_array
+    end
+
 end
 
 my_n_array = [1, 2, 3, 4]
@@ -28,3 +41,13 @@ end
 my_n_array.my_each_with_index do |x,index|
     puts "#{x} , #{index}"
 end
+
+# my_n_array.select { |x| puts x == 1}
+
+
+# TEST for my_select
+numbers = [1, 2, 3, 4, 5, 6]
+p numbers.my_select { |x| x.even? }
+p numbers.my_select { |x| x.odd?}
+
+p numbers.my_all { |x| x > 2}
