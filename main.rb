ea861818a45cc487ede66a1aebe2344f8d275b0d
nyan lin tun <devkonyan@gmail.com>
2019-11-29 04:46:02 +0630

    new : my_each_with_index feature done

--- main.rb
+++ main.rb
@@ -2,8 +2,16 @@ module Enumerable
     def my_each
         i=0;
         while i < self.size
-            # yield to_a[i] if block_given
             yield(self[i])
+            i+=1
+        end
+        self
+    end
+
+    def my_each_with_index
+        i=0
+        while i< self.size
+            yield(self[i],i)
             i+=1
         end
         self
@@ -15,4 +23,8 @@ my_n_array = [1, 2, 3, 4]
 
 my_n_array.my_each do |x|
     puts x
+end
+
+my_n_array.my_each_with_index do |x,index|
+    puts "#{x} , #{index}"
 end
